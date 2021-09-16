da0a65050a077d42e91c8366cb9107e3ca8aa7ae
belo4ya <41exey.kov41ev@gmail.com>
2021-09-17 00:24:40 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const Mustache = require('mustache')
+const fs = require('fs')
+
+
+const generateReadMe = (view) => {
+    const template = './dist/main.mustache'
+    fs.readFile(template, (err, data) => {
+        if (err) throw err
+        const rendered = Mustache.render(data.toString(), view)
+        fs.writeFileSync('README.md', rendered)
+    })
+}
+
+
+const getPassedDays = () => {
+    const AD = new Date(2021, 8, 16)  // 16.09.2021 - нулевая дата
+    const now = new Date()
+    return Math.floor((now - AD) / (1000 * 3600 * 24))
+}
+
+
+const view = {
+    days: getPassedDays()
+}
+
+generateReadMe(view)
