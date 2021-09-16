2c79932fdafc320cce5d5103bf524a5918003938
belo4ya <41exey.kov41ev@gmail.com>
2021-09-17 00:55:13 +0300

    src dist

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const generateReadMe = (view) => {
 const getPassedDays = () => {
     const AD = new Date(Date.UTC(2021, 8, 16))  // 16.09.2021 - нулевая дата
     const now = new Date()
-    return ((now - AD) / (1000 * 3600 * 24))
+    return Math.floor((now - AD) / (1000 * 3600 * 24))
 }
 
 
