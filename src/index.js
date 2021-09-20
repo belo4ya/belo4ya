ccaab7269f7fb90e74a69e6fded33e467db9a975
belo4ya <41exey.kov41ev@gmail.com>
2021-09-20 22:59:18 +0300

    node modules

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
-const Mustache = require('mustache');
-const fs = require('fs');
+import mustache from "mustache/mustache.mjs";
+import fs from 'fs';
 
 
 const generateReadMe = (view) => {
     const template = './dist/readme.mustache';
     fs.readFile(template, (err, data) => {
         if (err) throw err;
-        fs.writeFileSync('README.md', Mustache.render(data.toString(), view));
+        fs.writeFileSync('README.md', mustache.render(data.toString(), view));
     });
-}
+};
 
 
 const getPassedDays = (startDate) => {
     const now = new Date();
     const offset = 3 * 60 * 60 * 1000;
     return Math.floor((now - startDate + offset) / (24 * 60 * 60 * 1000));
-}
+};
 
 
 const startDate = new Date(Date.UTC(2021, 8, 16));
 
 const view = {
-    startDate: startDate.toLocaleDateString('en-GB', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit'
-    }).replace(new RegExp('/', 'g'), '.'),
+    startDate: startDate.toLocaleDateString('ru-RU'),
     days: getPassedDays(startDate)
 };
 
